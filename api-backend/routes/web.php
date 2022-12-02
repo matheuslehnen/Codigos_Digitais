4cfae3af3ca219d7b1273b461c9f2afab7b647d4
Matheus Lehnen <matheuslehnen@hotmail.com>
2022-12-02 07:22:53 -0300

    .

--- api-backend/routes/web.php
+++ api-backend/routes/web.php
@@ -18,24 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-//Route::post('usuario', [UsuarioController::class, 'create']);
-//Route::get('usuario/{email}', [UsuarioController::class, 'show']);
-//Route::get('usuario', [UsuarioController::class, 'index']);
-//
-//Route::post('/cliente', [ClienteController::class, 'create']);
-//Route::get('/cliente/{email}', [ClienteController::class, 'show']);
-//Route::get('/cliente', [ClienteController::class, 'index']);
-//
-//Route::post('/produto', [ProdutoController::class, 'create']);
-//Route::get('/produto/{email}', [ProdutoController::class, 'show']);
-//Route::get('/produto', [ProdutoController::class, 'index']);
-//
-//Route::post('/fornecedor', [FornecedorController::class, 'create']);
-//Route::get('/fornecedor/{email}', [FornecedorController::class, 'show']);
-//Route::get('/fornecedor', [FornecedorController::class, 'index']);
-//
-//Route::post('/orcamento', [OrcamentoController::class, 'create']);
-//Route::get('/orcamento/{email}', [OrcamentoController::class, 'show']);
-//Route::get('/orcamento', [OrcamentoController::class, 'index']);
-
